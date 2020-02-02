60905c1e112998cf7ed6292716d0f71e2f31cad7
tavaresm1 <tavaresm1@gmail.com>
2020-02-02 18:56:27 -0500

    Change log for schema delploy tested

--- DB_Design/Liquibase/liquibase.bat
+++ DB_Design/Liquibase/liquibase.bat
@@ -30,4 +30,4 @@ IF NOT DEFINED JAVA_OPTS set JAVA_OPTS=
 set JAVA_PATH=""
 if NOT "%JAVA_HOME%" == "" set JAVA_PATH=%JAVA_HOME%\bin\
 
-"C:\Program Files (x86)\Java\jre1.8.0_241\bin\java" -cp "%CP%" %JAVA_OPTS% liquibase.integration.commandline.Main %*
+"C:\Program Files\Java\jdk-13.0.2\bin\java.exe" -cp "%CP%" %JAVA_OPTS% liquibase.integration.commandline.Main %*
