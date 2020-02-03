e2dc4aace39eeec516a1da4e27cd503020b8980c
Jack Blakely <jack@pop-os.localdomain>
2020-02-02 23:42:28 -0500

    Docker working but on port 80 only

---
+++ scripts/CreateAllDockerImages.sh
@@ -0,0 +1,2 @@
+docker build -t localhost/shooter:latest ./shooter/shooterapi --no-cache
+docker system prune -f
---
+++ scripts/RunDockerContainer.sh
@@ -0,0 +1,1 @@
+docker run -p 5000:5000 -p 5001:5001 -p 80:80 localhost/shooter:latest
