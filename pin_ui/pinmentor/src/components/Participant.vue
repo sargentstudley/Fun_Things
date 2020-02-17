34bf9829a6efcafe677fee1fe8b6046d0ae1e30b
Matthew Studley <sarge@Matthews-iMac.local>
2020-02-17 15:35:43 -0500

    Added participant vue component  - Includes first name and last name.

---
+++ pin_ui/pinmentor/src/components/Participant.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="participant">
+        First Name: {{ firstName }}
+        Last Name: {{ lastName }}
+    </div>
+</template>
+<script>
+export default {
+   name:'Participant',
+   props: {
+       firstName: String,
+       lastName: String
+   }
+}
+</script>
---
+++ pin_ui/pinmentor/tests/unit/participant.spec.js
@@ -0,0 +1,24 @@
+import { expect } from 'chai';
+import { shallowMount } from '@vue/test-utils';
+import Participant from '@/components/Participant.vue';
+
+describe("Participant component", () => {
+    it("has a first name", () => {
+        const firstName = 'John';
+
+        const wrapper = shallowMount(Participant, {
+            propsData: { firstName }
+        });
+
+        expect(wrapper.text()).to.include(firstName);
+    });
+
+    it("has a last name", () => {
+        const lastName = 'Doe';
+        const wrapper = shallowMount(Participant, {
+            propsData: { lastName }
+        });
+
+        expect(wrapper.text()).to.include(lastName);
+    });
+});
