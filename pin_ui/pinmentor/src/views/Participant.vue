40d19e2d37b05f6dde4246d06764acf8fe64bfdf
Matthew Studley <sarge@Matthews-iMac.local>
2020-02-23 22:52:30 -0500

    Added initial fetch logic - that I will probably delete.

--- pin_ui/pinmentor/src/views/Participant.vue
+++ pin_ui/pinmentor/src/views/Participant.vue
@@ -4,11 +4,45 @@
     landing page for participants to view themselves and their progress. Note:
     import individual participantt components, then they can be added just like
     an HTML tag here!
+    <ul>
+      <Participant firstName="John" lastName="Doe"/>
+    </ul>
   </div>
 </template>
 
 <script>
-export default {};
+import Participant from '../components/Participant';
+
+export default {
+  name: 'ParticipantList',
+  data: () => {
+    return {
+      participants: []
+    }
+  },
+  components: {
+    Participant,
+  },
+  methods: {
+    fetchData: () => {
+      let self = this;
+      // This needs to be replaced with an ENV variable that gets injected on-build, or something equivilent.
+      // Request currently doesn't work because CORS violation (in this case 192.168.99.100 is my docker host). 
+      // This is where Ngnix might come in very handy. 
+      // Also, check Axios - seems a common framework for Vue and API interaction.
+      const myRequest = new Request('http://192.168.99.100:9090/participant/');
+      fetch(myRequest)
+        .then((response) => {return response.json()})
+        .then((data) => {
+          self.participants = data;
+        }).catch(error => { console.log(error); });
+    }
+  },
+  mounted() {
+    this.fetchData();
+  }
+};
 </script>
 
 <style scoped></style>
+
